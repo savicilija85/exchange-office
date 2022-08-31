d3ab15203545ca8128e9ca7cc04631b2fa1f14e7
Ilija Savic <savic.ilija85@gmail.com>
2022-08-31 19:03:11 +0200

    Fixed Docker issues, preparing Laravel databases

--- init.sh
+++ init.sh
@@ -5,6 +5,5 @@ touch docker/logs/cron/cron.log
 mkdir -p docker/logs/nginx
 touch docker/logs/nginx/access.log
 touch docker/logs/nginx/errors.log
-mkdir -p docker/data/mysql
 mkdir -p docker/composer_cache
 mkdir -p docker/npm_cache
